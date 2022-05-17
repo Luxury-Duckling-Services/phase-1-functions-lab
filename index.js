c1e2fddce50a41a3639ce1073a585b317b8a55c1
Mark <marksreese@gmail.com>
2022-05-17 15:25:28 -0700

    commit tree 8581e78f110c3b18bcc660b42921d2329a6df310

--- index.js
+++ index.js
@@ -17,22 +17,19 @@ function calculatesFarePrice(start, dest){
         //first 400 feet are free
         case (dist <= 400):
             return 0;
-            break;
         //400-2000 feet = $0.02/foot
         case (dist <= 2000):
-            const fullFare = dist * 0.02;
-            const fare = fullFare - 8;
-            const truncatedFare = fare.toFixed(2);
-            const parsedFare = parseFloat(truncatedFare);
-            return parsedFare;
-            break;
-        //2000-2500 feet = flat fare - how much?
+            //const fullFare = dist * 0.02;
+            //const fare = fullFare - 8;
+            //const truncatedFare = fare.toFixed(2);
+            //const parsedFare = parseFloat(truncatedFare);
+            //return parsedFare;
+            return (dist - 400) * 0.02;
+        //2000-2500 feet = flat fare of $25
         case (dist <= 2500):
             return 25;
-            break;
         //>2500 feet returns "cannot travel that far"
         case (dist > 2500):
             return `cannot travel that far`;
-            break;
     }
 }
